5b26b513688ff4ce70d204d22f577987f4a2f034
Dênnis Dantas <ddspog@gmail.com>
2023-03-30 22:44:36 -0300

    Make tool work with list of expressions
    1. User enters expression and presses enter
    2. The expression is split by the semicolon and evaluated by the math-expression-evaluator
    3. The result is shown in the list

--- src/index.tsx
+++ src/index.tsx
@@ -3,45 +3,67 @@ import Mexp from "math-expression-evaluator";
 import { useState } from "react";
 
 function evaluateExpression(expression: string) {
-  const mext = new Mexp();
+    const mext = new Mexp();
 
-  try {
-    return mext.eval(expression, [], { precision: 2 });
-  } catch {
-    return null;
-  }
+    try {
+        return mext.eval(expression, [], { precision: 2 });
+    } catch {
+        return null;
+    }
+}
+
+type Roll = {
+    expression: string;
+    result: number | null;
 }
 
 export default function RPGRoll() {
-  const [expression, setExpression] = useState<string | null>(null);
-  const [result, setResult] = useState<number | string | null>(null);
+    const [expression, setExpression] = useState<string | null>(null);
+    const [instruction, setInstruction] = useState<string | null>(null);
+    const [rolls, setRolls] = useState<Roll[] | null>(null);
 
-  const handleOnTextChange = (value = "") => {
-    if (value == "") return;
-    setExpression(value);
-    setResult("Press enter to Roll");
-  };
+    const handleOnTextChange = (value = "") => {
+        if (value == "") return;
+        setExpression(value);
+        setInstruction("Press enter to Roll");
+    };
 
-  const rollDices = () => {
-    if (expression) setResult(evaluateExpression(expression));
-  };
-
-  return (
-    <List
-      filtering={false}
-      onSearchTextChange={handleOnTextChange}
-      navigationTitle="Dice Expression"
-      searchBarPlaceholder="Input the dice expression to calculate i.e. d20 + 3"
-    >
-      <List.Item
-        title={"Exp: " + expression}
-        subtitle={result ? "=> " + result : "Invalid Expression"}
-        actions={
-          <ActionPanel title="Dice Expression">
-            <Action title="Roll" onAction={rollDices} />
-          </ActionPanel>
+    const rollDices = () => {
+        if (expression) {
+            const rolls: Roll[] = expression.split(";").map((rollExp) => {
+                return {
+                    expression: rollExp,
+                    result: evaluateExpression(rollExp)
+                };
+            });
+            setRolls(rolls);
         }
-      />
-    </List>
-  );
+    };
+
+    return (
+        <List
+            filtering={false}
+            onSearchTextChange={handleOnTextChange}
+            navigationTitle="Dice Expression"
+            searchBarPlaceholder={instruction ? instruction : "Input the dice expression to calculate i.e. d20 + 3"}
+            actions={
+                <ActionPanel title="Dice Expression">
+                    <Action title="Roll" onAction={rollDices} />
+                </ActionPanel>
+            }
+        >
+            {rolls?.map((roll) => (
+                <List.Item
+                    key={roll.expression}
+                    title={"Exp: " + roll.expression}
+                    subtitle={roll.result ? "=> " + roll.result : "Invalid Expression"}
+                    actions={
+                        <ActionPanel title="Dice Expression">
+                            <Action title="Roll" onAction={rollDices} />
+                        </ActionPanel>
+                    }
+                />
+            ))}
+        </List>
+    );
 }
